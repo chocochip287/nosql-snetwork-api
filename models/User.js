41b9fafe4ee675b870c7875f387052922d292377
Matt <39172048+magiccreb@users.noreply.github.com>
2023-02-08 13:43:07 -0500

    Added mongoose package. Reconfigured server.js and connected it to config/connection.js. Set up userSchema through email and its validation.

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 // more to come
 
+// Import Mongoose
+const mongoose = require("mongoose");
+
 // username (string, unique, required, trimmed)
 
 // email (string, required, unique, must match a valid email address (look into Mongoose's matching validation))
@@ -7,3 +10,20 @@
 // thoughts (array of _id values referencing the Thought model)
 
 // friends (array of _id values referencing the User model (self-reference))
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, 
+        validate: {
+            // email validator that simply looks to see that the provided email is one or more of any character @ one or more of any characters . one or more of any number of characters. Not very specific or sophisticated but does the job.
+            validator: function(v) {
+                return /^.+@.+\..+$/.test(v);
+            },
+            message: newUser => `${newUser.email} is not a valid email address.`
+        },
+    },
+    // thoughts,
+    
+    // friends,
+    
+});
--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 // more to come
 // Imports express
 const express = require("express");
+// Imports connection
+const db = require("./config/connection");
 // Imports mongodb
 const mongodb = require("mongodb").MongoClient;
 // Imports ObjectID() function
 const ObjectId = require("mongodb").ObjectId;
+// Model import
+// const { Item } = require("./models");
 
 const app = express();
 const PORT = 6505;
 
 // Connection string for local MongoDB instance
 
-const connectionStringURI = "mongodb://127.0.0.1:27017/snetworkDB";
-
-// Connection variable
-
-let db;
-
-// Connection to MongoDB instance
-
-mongodb.connect(connectionStringURI, { useNewUrlParser: true, useUnifiedTopology: true },
-    (err, client) => {
-        // use client.db() constructor to add a new DB instance
-        db = client.db();
-        app.listen(PORT, () => {
-            console.log(`Listening at http://localhost:${PORT}`);
-        });
-    }
-);
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server is listening on port ${PORT}.`);
+    })
+})
 
 app.use(express.json());
