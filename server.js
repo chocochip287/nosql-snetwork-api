d294a44a8071aedd01a534b70002412f39275e17
Matt <39172048+magiccreb@users.noreply.github.com>
2023-02-07 23:30:43 -0500

    Began setup of server.js with express and mongodb imports. Installed packages and updated gitignore to ignore package-lock.

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 // more to come
+
+const express = require("express");
+const mongodb = require("mongodb").MongoClient;
+
+const app = express();
+const PORT = 6505;
+
+// Connection string for local MongoDB instance
+
+const connectionStringURI = "mongodb://127.0.0.1:27017/snetworkDB";
+
+// Connection variable
+
+let db;
+
+// Connection to MongoDB instance
+
+mongodb.connect(connectionStringURI, { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+        // use client.db() constructor to add a new DB instance
+        db = client.db();
+        app.listen(PORT, () => {
+            console.log(`Listening at http://localhost:${PORT}`);
+        });
+    }
+);
+
+app.use(express.json());
